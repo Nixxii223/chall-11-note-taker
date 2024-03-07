bd636b39dd910f15a7be3f49c1c2fc0e83703d52
nickib223 <146459728+nickib223@users.noreply.github.com>
2024-03-06 21:23:03 -0500

    bug

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // To parse JSON bodies
 app.use(express.static('public')); // Serve static files
-// app.use('/', routes); // Use the API routes
+app.use('/', routes); // Use the API routes
 
 // Serve index.html and notes.html
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
