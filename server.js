c9fe16275f3fab4f15aadf3d3d012dcae167e47e
nickib223 <146459728+nickib223@users.noreply.github.com>
2024-03-06 20:41:31 -0500

    changed port to using env variable

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 
-app.listen(3000, () => console.log('App is listening on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('App is listening on port ' + (process.env.PORT || 3000)));
